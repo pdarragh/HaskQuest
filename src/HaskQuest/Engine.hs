596c628b0d646c964344a256da979b73e84bb1cf
Pierce Darragh <pierce.darragh@gmail.com>
2016-04-13 09:28:23 -0600

    Engine work
    Rewrote parts of the engine to handle errors better and such.

--- src/HaskQuest/Engine.hs
+++ src/HaskQuest/Engine.hs
@@ -23,20 +23,42 @@ data Engine = Engine {
 } deriving (Show)
 
 runGame :: Engine -> IO ()
-runGame (Engine r p i) = do
+runGame e = do
+    me <- gameStep e
+    case me of
+        Nothing -> runGame e
+        Just e' -> runGame e'
+
+gameStep :: Engine -> IO (Maybe Engine)
+gameStep (Engine r p i) = do
+    -- print (Engine r p i)
     print r
-    input <- getLine
+    input <- promptUser
     let action = parseChoice input
     case action of
         (Go s) -> if length matchedExits == 1 then
-                runGame (Engine (room $ head matchedExits) (Just r) i)
+                wrapEngine (Engine (room $ head matchedExits) (Just r) i)
             else
-                error "No matching room!"
+                gameError "No matching room!"
             where
                 inlined = filter ( (==) s . inline) (exits r)
                 aliased = filter (elem s . aliases) (exits r)
                 matchedExits = nub $ inlined ++ aliased
-        Back -> if isNothing p then
-                error "Cannot go back!"
+        Back -> if isNothing p || null (filter ( (==) (fromJust p) . room) (exits r)) then
+                gameError "Cannot go back!"
             else
-                runGame (Engine (fromJust p) (Just r) i)
+                wrapEngine (Engine (fromJust p) (Just r) i)
+
+promptUser :: IO (String)
+promptUser = do
+    putStr "\n> "
+    input <- getLine
+    return input
+
+gameError :: String -> IO (Maybe Engine)
+gameError s = do
+    putStrLn $ s ++ "\n"
+    return Nothing
+
+wrapEngine :: Engine -> IO (Maybe Engine)
+wrapEngine e = return (Just e)
